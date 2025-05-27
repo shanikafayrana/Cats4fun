f24013e183dda1eb5c156da5cec279a4222f23b7
shanikafayrana <shanikafayrana@gmail.com>
2025-05-27 07:49:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,14 @@ st.write(
     "Yall i love cats lets start stanning cats guys and learn informatika hashtag slay -bi'sawesome"
 )
 st.image("IMG_4860.jpeg")
+
+
+
+st.title("Aplikasi Rawr")
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil") 
+angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
+
+if (angka % 2) == 0:
+    st.write(f"{angka} adalah Bilangan Genap")
+else:
+    st.write(f"{angka} adalah Bilangan Ganjil")
